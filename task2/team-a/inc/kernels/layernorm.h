16b43e9b6f51aa9f9a05488e86e334b9300e9655
ibmbt <ibrahimtahirbutt10@gmail.com>
2026-07-09 21:10:58 +0500

    layer norm function def

--- task2/team-a/inc/kernels/layernorm.h
+++ task2/team-a/inc/kernels/layernorm.h
@@ -3,6 +3,14 @@
 #include<stdio.h>
 #include<stdint.h>
 
-void layernorm();
+void layernorm(
+    const float* __restrict embed,
+    const float* __restrict weight,
+    const float* __restrict bias,
+    float* __restrict y,
+    size_t seq_len,
+    size_t embed_dim,
+    float eps
+);
 
 #endif
--- task2/team-a/src/kernels/layernorm.c
+++ task2/team-a/src/kernels/layernorm.c
@@ -1,5 +1,13 @@
 #include "../../inc/kernels/layernorm.h"
 
-void layernorm() {
-    // layernorm function
+void layernorm(
+    const float* __restrict embed,
+    const float* __restrict weight,
+    const float* __restrict bias,
+    float* __restrict y,
+    size_t seq_len,
+    size_t embed_dim,
+    float eps
+) {
+
 }
